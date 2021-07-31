bf82df00e835afcd8ce20db2ebb035984a65d65f
huangling <lingling374@qq.com>
2021-07-31 23:31:24 +0800

    build: fix build

--- src/extension.ts
+++ src/extension.ts
@@ -17,23 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
   //   vscode.commands.registerCommand("clab-lint.findAllI18N", findAllI18N)
   // );
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "clab-lint" is now active!');
-  vscode.window.showInformationMessage("Hello World from clab-lint!");
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "clab-lint.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from clab-lint!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
+  // vscode.window.showInformationMessage("Hello World from clab-lint!");
 
   let targetStringList: TargetStr[] = [];
   let finalLangObj: { [key: string]: any } = {};
