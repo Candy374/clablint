383cbdfbe1c9d959f71722563e79bfa87c5a8c0b
huangling <lingling374@qq.com>
2021-08-01 11:32:11 +0800

    fix: update check chinese key

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 const LANG_PREFIX = "src/i18n/";
 const DIR_ADAPTOR = "src/i18n/";
 const I18N_GLOB = `${LANG_PREFIX}**/*.ts`;
-const DOUBLE_BYTE_REGEX = /[^\x00-\xff]/g;
+const DOUBLE_BYTE_REGEX = /[\u4e00-\u9fa5]/g;
 
 export { LANG_PREFIX, I18N_GLOB, DOUBLE_BYTE_REGEX, DIR_ADAPTOR };
